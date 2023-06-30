6a7c230f29caf2e069557af7e2222743583486c8
Meriemi-git <meriemi@protonmail.com>
2023-06-30 09:56:44 +0200

    Clone proton-clang to build kernel

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 git apply --directory vendor/arrow vendor_arrow_0001-FP3-Set-msm8953-platform-for-QSSI.patch
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
