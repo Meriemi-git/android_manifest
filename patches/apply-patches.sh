af7e9e6f478e77412090580fc82ede6ef1806d7b
Meriemi-git <meriemi@protonmail.com>
2023-06-30 10:27:47 +0200

    Fetch vendor/extra large files

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git apply --directory vendor/arrow vendor_arrow_0001-FP3-Set-msm8953-platform-for-QSSI.patch
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
+cd vendor/extra && git lfs pull
